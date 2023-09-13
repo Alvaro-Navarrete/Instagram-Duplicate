43a94bbe0e5f305d899ab09c6609df5100bd288a
Alvaro Navarrete <alvaro.navarrete011@gmail.com>
2023-09-13 02:10:31 -0300

    segundo avance

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const menu = document.querySelector('.menu');
+const modal = document.querySelector('.modal');
+const wrapper = document.querySelector('.wrapper');
 
+menu.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+wrapper.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
