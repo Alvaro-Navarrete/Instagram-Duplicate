d3ffa8b172fb679895be44163fdd8340408c51df
Alvaro Navarrete <alvaro.navarrete011@gmail.com>
2023-10-19 20:53:59 -0300

    19/10

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-const menu = document.querySelector('.menu');
-const modal = document.querySelector('.modal');
-const wrapper = document.querySelector('.wrapper');
+// const menu = document.querySelector('.menu');
+// const modal = document.querySelector('.modal');
+// const wrapper = document.querySelector('.wrapper');
 
-menu.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.add('modal--show');
-});
+// menu.addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     modal.classList.add('modal--show');
+// });
 
-wrapper.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.remove('modal--show');
+// wrapper.addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     modal.classList.remove('modal--show');
+// });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const menu = document.querySelector('.modal');
+    const btnmenu = document.querySelector('.menu');
+
+    btnmenu.addEventListener("click", function () {
+        if (menu.style.display === "none" || menu.style.display === "") {
+            menu.style.display = "flex";
+        } else {
+            menu.style.display = "none";
+        }
+
+    });
+
+    // Cierra el menú si se hace clic en cualquier parte fuera de él
+    document.addEventListener("click", function (event) {
+        if (event.target !== btnmenu) {
+            menu.style.display = "none";
+        }
+    });
+
+
+    // Evita que se cierre el menú al hacer clic dentro de él
+    menu.addEventListener("click", function (event) {
+        event.stopPropagation();
+    });
+
 });
